8f2dcb58ad1c6bd8fb23c97eee707a226b795cb0
kashif-aly6466 <159264621+kashif-aly6466@users.noreply.github.com>
2024-03-14 05:47:44 -0700

    opperatorsALL

---
+++ arthermatic/arthe.js
@@ -0,0 +1,40 @@
+//  addition//
+let x = 100;
+let y = 300;
+let z = x+y;
+console.log("x =",x , " & y =",y)
+console.log("x+y = " ,x-y);
+console.log("x * y = " ,x*y);
+console.log("x ** y = " ,x**y);
+console.log("x / y = " ,x/y);
+console.log("x % y = " ,x%y);
+
+
+
+// unary opeerators(such as ! increment $ decrement)
+let a = 100;
+let b = 300;
+console.log("a =",a , " & b =",b)
+a++;
+console.log("a =",a)
+a--;
+console.log("a =",a)
+// Compreassion opperators//
+   console.log  ("100==300",a==b);   
+   
+   
+
+
+
+   let text = "";
+   let i = 0;
+   while (i < 10) {
+     text += "  " + i;
+     i++;
+   }
+   console.log(text);
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.querySelector('.display');
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
